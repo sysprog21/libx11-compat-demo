9e5af44e19d9d33c8ccc24b3d9def6acefe95726
jserv <jserv@users.noreply.github.com>
2026-08-05 06:26:04 +0000

    deploy: sysprog21/libx11-compat@4dfa927af8d3a4528bf3cf0ca06286fa6c62244e

--- mosaic.js
+++ mosaic.js
@@ -39,7 +39,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmp3xj4dvxd.js
+// include: /tmp/tmpo97sb9_s.js
 
   if (!Module.expectedDataFileDownloads) {
     Module.expectedDataFileDownloads = 0;
@@ -225,21 +225,21 @@ Module['FS_createPath']("/usr/share", "fonts", true, true);
 
   })();
 
-// end include: /tmp/tmp3xj4dvxd.js
-// include: /tmp/tmptcyh7up7.js
+// end include: /tmp/tmpo97sb9_s.js
+// include: /tmp/tmpchcaeyo3.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmptcyh7up7.js
-// include: /tmp/tmpirxolr99.js
+  // end include: /tmp/tmpchcaeyo3.js
+// include: /tmp/tmp8nd9pz5b.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpirxolr99.js
+  // end include: /tmp/tmp8nd9pz5b.js
 
 
 // Sometimes an existing Module object exists with properties
